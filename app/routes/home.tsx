4372b2c6a528cd651bb299099e024b1eb91ba98e
Aditi Sharma <aditisharmamay22@gmail.com>
2026-06-22 14:20:06 +0530

    modified the UI of homepage , added puter.ts file

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           <h2>Review your submission and check AI-powered feedback</h2>
         </div>
         {resumes.length > 0 && (
-          <div className="resume-cards-container">
+          <div className="resumes-section ">
             {resumes.map((resume) => (
               <ResumeCard key={resume.id} resume={resume} />
             ))}
